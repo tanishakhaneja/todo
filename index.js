ae82e1dc3682960baff162269ecbf7bcb676ac0a
tanishakhaneja <taniikhaneja@gmail.com>
2025-09-16 23:00:18 +0530

    update changes

--- index.js
+++ index.js
@@ -1,62 +1,52 @@
 const express = require("express");
-const path = require("path");
-const methodOverride = require("method-override");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(require("method-override")("_method"));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "views");
 
 let todos = [
   { id: 1, task: "Learn EJS", priority: "High" },
-  { id: 2, task: "Build Todo app", priority: "Medium" },
+  { id: 2, task: "Build Todo app", priority: "Medium" }
 ];
 
 app.get("/", (req, res) => {
-  const filter = req.query.filter;
-  const filteredTodos = filter && filter !== "All"
-    ? todos.filter(t => t.priority === filter)
-    : todos;
-  res.render("index", { todos: filteredTodos, editTodo: null, filter: filter || "All" });
+  let shown = todos;
+  if (req.query.filter && req.query.filter !== "All") {
+    shown = todos.filter(t => t.priority === req.query.filter);
+  }
+  res.render("index", { todos: shown, editTodo: null, filter: req.query.filter || "All" });
 });
 
 app.post("/todos", (req, res) => {
-  const { task, priority } = req.body;
-  if (task && priority) {
-    const id = todos.length ? Math.max(...todos.map(t => t.id)) + 1 : 1;
-    todos.push({ id, task, priority });
+  if (req.body.task && req.body.priority) {
+    todos.push({
+      id: todos.length ? Math.max(...todos.map(t => t.id)) + 1 : 1,
+      task: req.body.task,
+      priority: req.body.priority
+    });
   }
   res.redirect("/");
 });
 
 app.get("/todos/:id/edit", (req, res) => {
-  const id = Number(req.params.id);
-  const editTodo = todos.find(t => t.id === id);
+  const editTodo = todos.find(t => t.id == req.params.id);
   res.render("index", { todos, editTodo, filter: "All" });
 });
 
 app.put("/todos/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const { task, priority } = req.body;
-  const todo = todos.find(t => t.id === id);
-  if (todo && task && priority) {
-    todo.task = task;
-    todo.priority = priority;
+  const todo = todos.find(t => t.id == req.params.id);
+  if (todo && req.body.task && req.body.priority) {
+    todo.task = req.body.task;
+    todo.priority = req.body.priority;
   }
   res.redirect("/");
 });
 
 app.delete("/todos/:id", (req, res) => {
-  const id = Number(req.params.id);
-  todos = todos.filter(t => t.id !== id);
+  todos = todos.filter(t => t.id != req.params.id);
   res.redirect("/");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(3001);
